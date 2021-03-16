10354cd0ce3ae6497ffe53ffdc109460a74b60ef
LManjitha <lmanjithah@gmail.com>
2021-03-16 14:11:45 +0530

    set upload evidence

--- app/src/main/java/com/example/careu/policeEcomplain.java
+++ app/src/main/java/com/example/careu/policeEcomplain.java
@@ -1,15 +1,45 @@
 package com.example.careu;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
+import android.content.ClipData;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Base64;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.ByteArrayOutputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class policeEcomplain extends AppCompatActivity {
+
+    Spinner districtSpinner,policeSpinner;
+    EditText note;
+    String uploadUrl = "http://10.0.2.2/careu-php/uploadID.php";
+    List<Bitmap> bitmaps = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,4 +64,71 @@ public class policeEcomplain extends AppCompatActivity {
         intent.setType("image/*");
         startActivityForResult(intent,1);
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==1 && resultCode== RESULT_OK && data != null){
+
+            ClipData clipData = data.getClipData(); //this will  be null if choose 1 image
+            if(clipData != null){
+                for(int i=0;i<clipData.getItemCount();i++){
+                    Uri imageUri = clipData.getItemAt(i).getUri();
+                    try {
+                        InputStream is = getContentResolver().openInputStream(imageUri);
+                        Bitmap bitmap = BitmapFactory.decodeStream(is);
+                        bitmaps.add(bitmap);
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }else{
+                Uri imageUri = data.getData();
+                try {
+                    InputStream is = getContentResolver().openInputStream(imageUri);
+                    Bitmap bitmap = BitmapFactory.decodeStream(is);
+                    bitmaps.add(bitmap);
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    private String imageToString(Bitmap bitmap){
+
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG,100,byteArrayOutputStream);
+        byte[] imgbytes = byteArrayOutputStream.toByteArray();
+        return Base64.encodeToString(imgbytes,Base64.DEFAULT);
+    }
+
+    StringRequest stringRequest = new StringRequest(Request.Method.POST, uploadUrl, new Response.Listener<String>() {
+        @Override
+        public void onResponse(String response) {
+            try{
+                JSONObject jsonObject = new JSONObject(response);
+                String Response = jsonObject.getString("response");
+            }catch (JSONException e){
+                e.printStackTrace();
+            }
+        }
+    }, new Response.ErrorListener(){
+        @Override
+        public void onErrorResponse(VolleyError error) {
+
+        }
+    })
+    {
+        @Override
+        protected Map<String, String> getParams() throws AuthFailureError {
+            Map <String,String> params = new HashMap<>();
+            params.put("userName","username");
+            params.put("name","2");
+            params.put("image",imageToString(bitmaps.get(1)));
+
+            return params;
+        }
+    };
+
 }
