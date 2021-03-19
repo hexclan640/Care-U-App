1c85f9dc80856eef053654013d2993bf66cb1f52
Pasanbashz <bashzpasan@gmail.com>
2021-03-19 12:59:14 +0530

    added_pop5Window

--- app/src/main/java/com/example/careu/Pop5.java
+++ app/src/main/java/com/example/careu/Pop5.java
@@ -1,6 +1,7 @@
 package com.example.careu;
 
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,5 +11,15 @@ public class Pop5 extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.pop5_window);
+
+        DisplayMetrics dm = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
+
+        int width =dm.widthPixels;
+        int height = dm.heightPixels;
+
+        getWindow().setLayout((int)(width*.8),(int)(height*.7));
     }
 }
