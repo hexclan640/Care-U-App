a4521f3db905d6cc8e790443aa483a21590ee188
Pasanbashz <bashzpasan@gmail.com>
2021-03-19 18:59:12 +0530

    added_pop6_window

--- app/src/main/java/com/example/careu/Pop6.java
+++ app/src/main/java/com/example/careu/Pop6.java
@@ -1,4 +1,25 @@
 package com.example.careu;
 
-public class Pop6 {
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Pop6 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.pop6_window);
+
+        DisplayMetrics dm = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
+
+        int width =dm.widthPixels;
+        int height = dm.heightPixels;
+
+        getWindow().setLayout((int)(width*.8),(int)(height*.7));
+    }
 }
