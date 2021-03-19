f7907720e335b0dec708d6370ec6e32aec6995a5
Pasanbashz <bashzpasan@gmail.com>
2021-03-19 19:25:56 +0530

    added_pop7_window

--- app/src/main/java/com/example/careu/Pop7.java
+++ app/src/main/java/com/example/careu/Pop7.java
@@ -1,6 +1,25 @@
 package com.example.careu;
 
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Pop7 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.pop7_window);
+
+        DisplayMetrics dm = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
+
+        int width =dm.widthPixels;
+        int height = dm.heightPixels;
+
+        getWindow().setLayout((int)(width*.8),(int)(height*.7));
+    }
 }
