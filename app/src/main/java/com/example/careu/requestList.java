83a03e42e15a01249c9111f48519e96225ce5f09
Damish <Damishnisal100@gmail.com>
2021-03-24 15:20:01 +0530

    get the status of the request

--- app/src/main/java/com/example/careu/requestList.java
+++ app/src/main/java/com/example/careu/requestList.java
@@ -1,11 +1,16 @@
 package com.example.careu;
 
+import androidx.annotation.ColorInt;
+import androidx.annotation.DrawableRes;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
@@ -14,7 +19,9 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -38,6 +45,7 @@ public class requestList extends AppCompatActivity {
     private static  String des[];
     private static String policeStation[];
     private  static String requestId[];
+    private  static String status[];
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -58,6 +66,7 @@ public class requestList extends AppCompatActivity {
                 rq.putExtra("policeStation",policeStation[i]);
                 rq.putExtra("description",des[i]);
                 rq.putExtra("requestId",requestId[i]);
+                rq.putExtra("status",status[i]);
                 startActivity(rq);
 //                Toast.makeText(requestList.this, date[i], Toast.LENGTH_SHORT).show();
             }
@@ -82,6 +91,7 @@ public class requestList extends AppCompatActivity {
                     des = new String[jsonArray.length()];
                     policeStation = new String[jsonArray.length()];
                     requestId = new String[jsonArray.length()];
+                    status= new String[jsonArray.length()];
 
                     for(int i=0; i< jsonArray.length();i++){
                         jsonObject = jsonArray.getJSONObject(i);
@@ -91,9 +101,10 @@ public class requestList extends AppCompatActivity {
                         policeStation[i] = jsonObject.getString("policeStation");
                         des[i] = jsonObject.getString("description");
                         requestId[i]=jsonObject.getString("requestId");
+                        status[i]=jsonObject.getString("status");
                     }
 
-                    MyAdapter myAdapter = new MyAdapter(getApplicationContext(),date,time,noP,policeStation,des,requestId);
+                    MyAdapter myAdapter = new MyAdapter(getApplicationContext(),date,time,noP,policeStation,des,requestId,status);
                     requestView.setAdapter(myAdapter);
 
             }catch (Exception ex){
@@ -137,8 +148,9 @@ public class requestList extends AppCompatActivity {
         String policeStation[];
         String des[];
         String requestId[];
+        String status[];
 
-         MyAdapter(Context c, String date[], String time[], String noP[],String policeStation[], String des[],String requestId[]) {
+         MyAdapter(Context c, String date[], String time[], String noP[],String policeStation[], String des[],String requestId[],String status[]) {
             super(c,R.layout.request_row,R.id.tv1,date);
 
             context = c;
@@ -148,6 +160,7 @@ public class requestList extends AppCompatActivity {
              this.policeStation = policeStation;
             this.des = des;
             this.requestId =requestId;
+            this.status =status;
         }
 
         @NonNull
@@ -157,17 +170,29 @@ public class requestList extends AppCompatActivity {
             LayoutInflater inflater = (LayoutInflater)getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
             View row=inflater.inflate(R.layout.request_row,parent,false);
 
+
+            LinearLayout l = row.findViewById(R.id.layoutview);
+            l.setBackground(getDrawable(R.drawable.button_background));
+//            l.setBackgroundColor(Color.argb(40,226, 11, 11));
+//            l.setBackground(getDrawable(R.drawable.button_background));
+
+//            l.setBackgroundColor(Color.argb(40,226, 11, 11));
+//            row.setBackgroundDrawable(getDrawable(R.drawable.background));
+//            row.setBackgroundColor(Color.argb(40,226, 11, 11));
+
             TextView tv0=row.findViewById(R.id.tv0);
             TextView tv1=row.findViewById(R.id.tv1);
             TextView tv2=row.findViewById(R.id.tv2);
             TextView tv3=row.findViewById(R.id.tv3);
             TextView tv4=row.findViewById(R.id.tv4);
+            TextView tv5=row.findViewById(R.id.tv5);
 
             tv0.setText("000000"+"-"+requestId[position]);
             tv1.setText(date[position]);
             tv2.setText(time[position]);
             tv3.setText(noP[position]);
             tv4.setText(des[position]);
+            tv5.setText(status[position]);
             return row;
         }
     }
